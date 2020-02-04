6e0d7d6ada0868fad431a0197075f77d8ea3c990
Rachel Williams <rachelwilliams@MacBook-Pro.local>
2020-02-03 22:21:23 -0600

    Updates

---
+++ precache-manifest.69afa2e835f2c5350ed4a2f1fc479bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7208b0486953af046f69f8488c27ca2",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "8ba68faf84e2cfdd651a",
+    "url": "/monstersrolodex/static/css/main.c3d45be4.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monstersrolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstersrolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba68faf84e2cfdd651a",
+    "url": "/monstersrolodex/static/js/main.d687f422.chunk.js"
+  },
+  {
+    "revision": "7866796e6fc4260a18cc",
+    "url": "/monstersrolodex/static/js/runtime-main.dbd6355d.js"
+  }
+]);
